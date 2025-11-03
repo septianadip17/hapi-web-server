8efa66c8f7b81bcd487fc3249d0f4b0ab103e60e
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-04 01:10:48 +0700

    routes file

---
+++ routes.js
@@ -0,0 +1,17 @@
+const routes = [
+  {
+    method: "GET",
+    path: "/",
+    handler: (request, h) => {
+      return "Homepage";
+    },
+  },
+  {
+    method: "GET",
+    path: "/about",
+    handler: (request, h) => {
+      return "About page";
+    },
+  },
+];
+module.exports = routes;
