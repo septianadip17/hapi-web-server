145a2c4829ba71503e22b7985011c28d382cbc9d
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-04 01:19:40 +0700

    test done

--- routes.js
+++ routes.js
@@ -7,11 +7,33 @@ const routes = [
     },
   },
   {
+    method: "*",
+    path: "/",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method tersebut";
+    },
+  },
+  {
     method: "GET",
     path: "/about",
     handler: (request, h) => {
       return "About page";
     },
   },
+  {
+    method: "*",
+    path: "/about",
+    handler: (request, h) => {
+      return "Halaman tidak dapat diakses dengan method";
+    },
+  },
+  {
+    method: "*",
+    path: "/{any*}",
+    handler: (request, h) => {
+      return "Halaman tidak ditemukan";
+    },
+  },
 ];
+
 module.exports = routes;
